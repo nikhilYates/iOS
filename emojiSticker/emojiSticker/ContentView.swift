12bdfc48ed649c906c8a4f909c564f1a3395e5f6
nikhilYates <nikhil.yates@outlook.com>
2024-07-05 15:11:32 -0400

    my first app

---
+++ emojiSticker/emojiSticker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  emojiSticker
+//
+//  Created by Nikhil Yates on 2024-07-05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Sup, gang")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ emojiSticker/emojiSticker/emojiStickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  emojiStickerApp.swift
+//  emojiSticker
+//
+//  Created by Nikhil Yates on 2024-07-05.
+//
+
+import SwiftUI
+
+@main
+struct emojiStickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
